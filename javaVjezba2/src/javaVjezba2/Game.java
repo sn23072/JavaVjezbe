dca6e41eba866d72eddbe26628cd7ff93ff2ef26
Sara Nikčević <sara.nikcevic@udg.edu.me>
2025-10-02 16:52:36 +0200

    Add files via upload

---
+++ javaVjezba2/src/javaVjezba2/Game.java
@@ -0,0 +1,12 @@
+package javaVjezba2;
+
+public class Game {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Sara Nikcevic 23072
+		//Andrea Markovic 23073
+	}
+
+}
